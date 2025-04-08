6ff2fc7c420f5ce7e1d7c62c84519c44d05e0893
HeitorDalla <heitorvillavilla@gmail.com>
2025-04-07 21:17:05 -0300

    Implementado o efeito de scroll behavior

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 "use strict";
 
+const links = [...document.querySelectorAll(".link")];
+
+// Implementar o efeito de scroll-behavior nos links do menu
+document.addEventListener("DOMContentLoaded", (event) => {
+    event.preventDefault();
+
+    links.forEach((link) => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+    
+            const elementoAtributo = link.getAttribute("atribute");
+            const secaoLink = document.getElementById(elementoAtributo);
+            if (secaoLink) {
+                secaoLink.scrollIntoView({behavior: 'smooth'});
+            }  
+        })
+    });
+});
+
